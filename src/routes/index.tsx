56bff78918cb36e379a042134f460eddfd80d826
PatrickJS <github@gdi2290.com>
2023-11-14 19:00:15 -0800

    chore: update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+export const Child = component$(() => {
+  return (
+    <>
+      <div>
+        hi again
+      </div>
+    </>
+  );
+});
 
 export default component$(() => {
   return (
     <>
-      <Hero />
-      <Starter />
-
       <div>
         hi
+        <Child />
       </div>
     </>
   );
