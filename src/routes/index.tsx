adcaaa3980efa67cf7edf6461e5e1aa654ad4750
PatrickJS <github@gdi2290.com>
2023-11-14 19:05:03 -0800

    feat: Weather cmp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,44 @@
-import { component$ } from "@builder.io/qwik";
+import { Resource, component$, useResource$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+
+export const Weather = component$(() => {
+  const store = useStore({
+    city: "",
+  });
+ 
+  const weatherResource = useResource$<any>(async ({ track, cleanup }) => {
+    const cityName = track(() => store.city);
+    const abortController = new AbortController();
+    cleanup(() => abortController.abort("cleanup"));
+    const res = await fetch(`http://weatherdata.com?city=${cityName}`, {
+      signal: abortController.signal,
+    });
+    const data = res.json();
+    return data;
+  });
+ 
+  return (
+    <div>
+      <input
+        name="city"
+        onInput$={(ev: any) => (store.city = ev.target.value)}
+      />
+      <Resource
+        value={weatherResource}
+        onResolved={(weather) => {
+          return <div>Temperature: {weather.temp}</div>;
+        }}
+      />
+    </div>
+  );
+});
 
 export const Child = component$(() => {
   return (
     <>
       <div>
         hi again
+        <Weather />
       </div>
     </>
   );
